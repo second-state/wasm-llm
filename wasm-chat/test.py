180440502e620355921cc74ec505b54aa90dbf16
Xin Liu <sam@secondstate.io>
2023-11-27 13:24:08 +0000

    chore(wasm-chat): update `test.py`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- wasm-chat/test.py
+++ wasm-chat/test.py
@@ -1,18 +1,20 @@
 from wasm_chat import *
+from pathlib import Path
 
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description='Wasm Chat')
-    parser.add_argument('--model_file', type=str, default='tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf',
+    parser.add_argument('model_file', type=str, default='tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf',
                         help='model file to run')
-    parser.add_argument('--wasm_file', type=str, help='Path to wasm-infer.wasm')
+    parser.add_argument('wasm_file', type=str, help='Path to wasm-infer.wasm')
     args = parser.parse_args()
 
 
-    model_file = "/Volumes/Dev/secondstate/me/pyo3/wasm-chat/tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf"
+    model_file = args.model_file # "tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf"
     model_alias = "default"
-    wasm_file = "/Volumes/Dev/secondstate/me/wasm-llm/target/wasm32-wasi/release/inference.wasm"
+    wasm_file = str(Path(args.wasm_file).resolve()) # "/home/ubuntu/workspace/wasm-llm/wasm-infer/target/wasm32-wasi/release/wasm_infer.wasm"
     dir_mapping = ".:."
 
     # init wasm environment
