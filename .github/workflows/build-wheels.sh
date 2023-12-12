3c8a537f63b12342d8700479c0d06bc0ca47c577
Xin Liu <sam@secondstate.io>
2023-12-12 15:05:45 +0800

    ci: update `build-wheels.sh`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -9,8 +9,8 @@ export PATH="$HOME/.cargo/bin:$PATH"
 for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
     # rm -rf /io/build/
     "${PYBIN}/pip" install -U maturin==1.4.0 maturin[patchelf]
-    "${PYBIN}/pip" list installed
-    "${PYBIN}/pip" show maturin
+    # "${PYBIN}/pip" list installed
+    # "${PYBIN}/pip" show maturin
     "${PYBIN}/maturin" build -i "${PYBIN}/python" -r -o dist
     # "${PYBIN}/pip" install -U setuptools setuptools-rust wheel
     # "${PYBIN}/pip" wheel /io/ -w /io/dist/ --no-deps
@@ -21,7 +21,8 @@ done
 #     auditwheel repair "$whl" -w /io/dist/
 # done
 
-# # Install packages and test
-# for PYBIN in /opt/python/cp{36,37,38,39,310}*/bin; do
-#     "${PYBIN}/pip" install html-py-ever -f /io/dist/
-# done
+# Install packages and test
+for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
+    "${PYBIN}/pip" install pytest
+    "${PYBIN}/pytest" -v
+done
