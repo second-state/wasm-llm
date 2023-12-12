d75e0b006478c5377a410191ca3f3bf07590a5ed
Xin Liu <sam@secondstate.io>
2023-12-12 15:46:38 +0800

    ci: update `build-wheels.sh`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -18,13 +18,29 @@ done
 
 ls -al dist/
 
-# # Bundle external shared libraries into the wheels
-# for whl in dist/*{cp36,cp37,cp38,cp39,cp310}*.whl; do
-#     auditwheel repair "$whl" -w /io/dist/
-# done
-
-# Install packages and test
-for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
-    "${PYBIN}/pip" install pytest
-    "${PYBIN}/pytest" -v
+# Bundle external shared libraries into the wheels
+for whl in dist/*cp{38,39,310,311,312}*.whl; do
+    auditwheel repair "$whl" -w dist/
 done
+
+ls -al dist/
+
+PYBIN=(/opt/python/cp38*/bin, /opt/python/cp39*/bin)
+WHL=(wasm_chat-*cp38-cp38*.whl, wasm_chat-*cp39-cp39*.whl)
+# Get the length of the arrays
+length=${#PYBIN[@]}
+
+# Iterate over the arrays
+for ((i=0; i<$length; i++)); do
+    "${PYBIN[$i]}/pip" install dist/${WHL[$i]} --force-reinstall
+    "${PYBIN[$i]}/pip" install pytest
+    "${PYBIN[$i]}/pytest" -v
+    # echo "${PYBIN[$i]} - ${array2[$i]}"
+done
+
+# # Install packages and test
+# for PYBIN in /opt/python/cp{38,39,310,311,312}*/bin; do
+#     "${PYBIN}/pip" install dist/${{ env.PACKAGE_NAME }}-0.1.0-cp38-cp38-*_x86_64.whl --force-reinstall
+#     "${PYBIN}/pip" install pytest
+#     "${PYBIN}/pytest" -v
+# done
